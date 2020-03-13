66fc565c010ee1c037be656402ce6c733d7aa169
HnThang <hnthangit@gmail.com>
2020-03-14 00:11:54 +0700

    fix: remove unused package
    Remove package org.apache.catalina.mapper in ManufacturerServiceImpl.java

--- src/main/java/com/shopping/service/impl/ManufacturerServiceImpl.java
+++ src/main/java/com/shopping/service/impl/ManufacturerServiceImpl.java
@@ -1,19 +1,12 @@
 package com.shopping.service.impl;
 
-import java.lang.reflect.Type;
-import java.util.List;
-import java.util.Optional;
-
-import org.apache.catalina.mapper.Mapper;
 import org.modelmapper.ModelMapper;
-import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import com.shopping.dao.IManufacturerDAO;
-import com.shopping.dto.ListDTO;
 import com.shopping.entity.Manufacturer;
 import com.shopping.service.IManufacturerService;
 import com.shopping.util.PageModel;
